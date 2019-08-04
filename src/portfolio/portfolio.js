e7af20e44efc027349779dd74af2cf33af257d29
Ramu151 <50612291+Ramu151@users.noreply.github.com>
2019-08-05 00:03:43 +0530

    faker

--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -1,9 +1,9 @@
-import React, { Component, useEffect, useState} from "react";
+import React, { Component, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { model_portfolios as model } from "./data";
 import "./style.css";
-
-const {Provider, Consumer} = React.createContext();
+//add sass and img faker module
+const { Provider, Consumer } = React.createContext();
 
 const PortFolio = () => {
   //const = useState()
@@ -11,7 +11,7 @@ const PortFolio = () => {
     console.log(ele, arr);
     return (
       <Provider value={ele}>
-        <CheckContext name="suresh"/>
+        <CheckContext name="suresh" />
       </Provider>
     );
   };
@@ -23,27 +23,33 @@ const PortFolio = () => {
   return <main>{template}</main>;
 };
 
-const CheckContext = (props) => {
-  console.log("ON",props);
-  return <Consumer>{ (context) => (
-    console.log('context',context),
-        <section className="pfSection">
-          <img src="" alt="" />
-          <title>{context.name}</title>
-          <div className="pfAttrElement">
-            <div className="attrLabelClass">volatility</div>
-            <div className="attrValueClass">{context.volatility}</div>
-          </div>
-          <div className="pfAttrElement">
-            <div className="attrLabelClass">currency</div>
-            <div className="attrValueClass">{context.currency}</div>
-          </div>
-          <div className="pfAttrElement">
-            <div className="attrLabelClass">mean_return</div>
-            <div className="attrValueClass">{context.mean_return}</div>
-          </div>
-        </section>
-  )}</Consumer>
-}
+const CheckContext = props => {
+  console.log("ON", props);
+  return (
+    <Consumer>
+      {context => (
+        console.log("context", context),
+        (
+          <section className="pfSection">
+            <img src="" alt="" />
+            <title>{context.name}</title>
+            <div className="pfAttrElement">
+              <div className="attrLabelClass">volatility</div>
+              <div className="attrValueClass">{context.volatility}</div>
+            </div>
+            <div className="pfAttrElement">
+              <div className="attrLabelClass">currency</div>
+              <div className="attrValueClass">{context.currency}</div>
+            </div>
+            <div className="pfAttrElement">
+              <div className="attrLabelClass">mean_return</div>
+              <div className="attrValueClass">{context.mean_return}</div>
+            </div>
+          </section>
+        )
+      )}
+    </Consumer>
+  );
+};
 
 ReactDOM.render(<PortFolio />, document.getElementById("root"));
