3d0a89d14ec1e8f7ce9f110f5b816cc8cba205ee
Ramu151 <50612291+Ramu151@users.noreply.github.com>
2024-03-19 07:14:41 +0000

    update

--- src/Comments.js
+++ src/Comments.js
@@ -1,24 +1,13 @@
 import { React, useState} from 'react'
-import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
-import Post from './Post';
 
-const App = () =>  {
+function App() {
   // eslint-disable-next-line
   const [title, setTitle] = useState('')
-  const [postData, setPostData] = useState(null)
   const handleChange = (e) => {
     console.log(e?.target?.value);
     setTitle(e?.target?.value)
-  }
-  const baseURL = 'https://silver-space-capybara-9jr9x4xpprqcv4g-4000.app.github.dev/posts'
-  const handleSubmit = async() => {
-    event.preventDefault()
-    axios.post(baseURL, { title }).then((response) => {
-      console.log(response.data)
-      setPostData(response.data);
-    });
   }
   return (
     <div className="App">
@@ -27,13 +16,13 @@ const App = () =>  {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <form onSubmit={handleSubmit}>
+        <form>
           <label>Post Title</label>
           <input type='text' onChange={handleChange}/>
-          <input type="submit" value="Submit"/>
+          <input type='button' value="submit"/>
         </form>
+
       </header>
-      <Post postData={postData} />
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import { React, useState} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+const Post = (props) => {
+    console.log("post", props)
+  return (
+    <div className="Post">
+
+    </div>
+  );
+}
+
+export default Post;
